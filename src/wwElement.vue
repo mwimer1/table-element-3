8f273a37e74f5dcc2b5bd4e64afaefd14fac90f0
mwimer1 <macrae@getcorso.com>
2024-07-16 19:18:47 -0500

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,93 +1,115 @@
 <template>
-  <a-table
-    :columns="columns"
-    :row-key="record => record.login.uuid"
-    :data-source="dataSource"
-    :pagination="pagination"
-    :loading="loading"
-    @change="handleTableChange"
-  >
-    <template #bodyCell="{ column, text }">
-      <template v-if="column.dataIndex === 'name'">{{ text.first }} {{ text.last }}</template>
+  <a-table :columns="columns" :data-source="dataSource">
+    <template #headerCell="{ column }">
+      <template v-if="column.key === 'name'">
+        <span>
+          <smile-outlined />
+          Name
+        </span>
+      </template>
+    </template>
+
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'name'">
+        <a>
+          {{ record.name }}
+        </a>
+      </template>
+      <template v-else-if="column.key === 'tags'">
+        <span>
+          <a-tag
+            v-for="tag in record.tags"
+            :key="tag"
+            :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+          >
+            {{ tag.toUpperCase() }}
+          </a-tag>
+        </span>
+      </template>
+      <template v-else-if="column.key === 'action'">
+        <span>
+          <a>Invite — {{ record.name }}</a>
+          <a-divider type="vertical" />
+          <a>Delete</a>
+          <a-divider type="vertical" />
+          <a class="ant-dropdown-link">
+            More actions
+            <down-outlined />
+          </a>
+        </span>
+      </template>
     </template>
   </a-table>
 </template>
 
 <script setup>
-import { computed, defineComponent, ref } from 'vue';
-import { usePagination } from 'vue-request';
-import axios from 'axios';
+import { defineComponent, ref } from 'vue';
+import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
 
-const AntTableElement = defineComponent({
-  name: 'AntTableElement',
-  props: {
-    columns: {
-      type: Array,
-      required: true,
-      default: () => [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          sorter: true,
-          width: '20%',
-        },
-        {
-          title: 'Gender',
-          dataIndex: 'gender',
-          filters: [
-            {
-              text: 'Male',
-              value: 'male',
-            },
-            {
-              text: 'Female',
-              value: 'female',
-            },
-          ],
-          width: '20%',
-        },
-        {
-          title: 'Email',
-          dataIndex: 'email',
-        },
-      ],
-    },
-    dataSource: {
-      type: Array,
-      required: true,
-      default: () => [],
-    },
-    pagination: {
-      type: Object,
-      required: true,
-      default: () => ({
-        total: 200,
-        current: 1,
-        pageSize: 10,
-      }),
-    },
+const columns = ref([
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
   },
-  setup(props) {
-    const { columns, dataSource, pagination } = props;
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+  },
+]);
 
-    const loading = ref(false);
+const dataSource = ref([
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+]);
 
-    const handleTableChange = (pag, filters, sorter) => {
-      // Custom logic to handle table changes (pagination, filters, sorting)
-      console.log('Table changed', pag, filters, sorter);
-    };
-
+export default defineComponent({
+  name: 'AntTableElement',
+  components: {
+    SmileOutlined,
+    DownOutlined,
+  },
+  setup() {
     return {
       columns,
       dataSource,
-      pagination,
-      loading,
-      handleTableChange,
     };
   },
 });
-
-export default AntTableElement;
 </script>
 
 <style scoped>
